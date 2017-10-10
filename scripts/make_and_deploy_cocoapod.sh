dfceed36bcc66e661f8c54f100afa26bf26f35cc
sundar05 <itssundar05@yahoo.co.in>
2017-10-10 13:12:46 +0530

    Update make_and_deploy_cocoapod.sh

--- scripts/make_and_deploy_cocoapod.sh
+++ scripts/make_and_deploy_cocoapod.sh
@@ -1,1 +1,17 @@
 
+    # Clean
+    IOS_FRAMEWORK_DIR="${PROJECTDIR}/sundar"
+
+    # rm -rf "${IOS_FRAMEWORK_BUILDDIR}"
+    mkdir -p "${IOS_FRAMEWORK_BUILDDIR}"
+
+    if [ -d "${SRCROOT}/build" ]; then
+        rm -rf "${SRCROOT}/build"
+    fi
+
+    # Build
+    cd "${PROJECTDIR}"
+    FRAMEWORK_CONFIG="Release"
+    PROJECT_NAME="swift"
+
+    xcodebuild -workspace "${IOS_FRAMEWORK_WORKSPACE_NAME}" -scheme "${IOS_FRAMEWORK_SCHEME_NAME}" -configuration "${FRAMEWORK_CONFIG}" -arch i386   only_active_arch=no defines_module=yes -sdk "iphonesimulator"  BUILD_DIR="${IOS_FRAMEWORK_BUILDDIR}/32bit"
